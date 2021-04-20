dff7dd007d16db629d56a5d6ba1dfa74cb8d86c3
Udit Karode <udit.karode@gmail.com>
2021-04-20 21:08:42 +0530

    Main: prevent directory traversal

--- Main.hs
+++ Main.hs
@@ -13,6 +13,9 @@ import Control.Monad.IO.Class
 import Data.UUID.V4
 import System.Directory
 import System.Console.ANSI
+import Data.Maybe (fromJust)
+import System.Path.NameManip (guess_dotdot, absolute_path)
+import Data.List (isPrefixOf)
 
 data Response = Response
   { status :: String
@@ -26,47 +29,61 @@ getResponse :: Bool -> String -> Response
 getResponse sts msg = Response { status = if sts then "success" else "failure", message = msg }
 
 -- always relative to project root
-pastesDir :: String
-pastesDir = "pastes"
+pastesDir :: IO String
+pastesDir = absolutize "pastes"
 
 getAvailableUuid :: IO String 
 getAvailableUuid = do
     uuid <- nextRandom
-    doesExist <- doesFileExist (makePath pastesDir (show uuid))
+    pdir <- liftIO pastesDir
+    doesExist <- doesFileExist (pdir ++ "/" ++ show uuid)
     if doesExist then getAvailableUuid else return $ show uuid
 
 makePath :: String -> String -> String
 makePath x y = x ++ "/" ++ y
 
+absolutize :: String -> IO String
+absolutize aPath = do
+        pathMaybeWithDots <- absolute_path aPath
+        return $ fromJust $ guess_dotdot pathMaybeWithDots
+
 app :: Server ()
 app = do
-    get root $ do
-        html "<h1>Batbin API</h1>"
+    get root $ html "<h1>Batbin API</h1>"
     post "/set" $ do
         pasteContent <- param' "content"
         if T.length pasteContent > 12000
              then S.json $ getResponse False "Paste too large!"
              else do
                 uuid <- liftIO getAvailableUuid
-                liftIO $ writeFile (makePath pastesDir uuid) (T.unpack pasteContent)
+                pdir <- liftIO pastesDir
+                liftIO $ writeFile (makePath pdir uuid) (T.unpack pasteContent)
                 S.json $ getResponse True uuid
     get "/get" $ do
         id <- param' "id"
         do
-            let filePath = makePath pastesDir id
-            doesExist <- liftIO $ doesFileExist filePath
-            if doesExist then
-                do 
-                    output <- liftIO $ readFile filePath
-                    text $ T.pack output
+            pdir <- liftIO pastesDir
+            filePath <- liftIO $ absolutize $ makePath pdir id
+            if pdir `Data.List.isPrefixOf` filePath then
+                do
+                    liftIO $ putStrLn filePath
+                    doesExist <- liftIO $ doesFileExist filePath
+                    if doesExist then
+                        do
+                            output <- liftIO $ readFile filePath
+                            text $ T.pack output
+                    else
+                        do
+                            S.json $ getResponse False "This paste does not exist!" 
             else
-                S.json $ getResponse False "This paste does not exist!"
+                do
+                    S.json $ getResponse False "Can't access pastes outside the paste dir!"
 
 main :: IO()
 main = do
     cfg <- defaultSpockCfg () PCNoDatabase ()
-    currentDir <- getCurrentDirectory
+    pdir <- liftIO pastesDir
     setSGR [SetColor Foreground Vivid Green]
-    putStrLn $ "pastes will be saved to the " ++ makePath currentDir pastesDir ++ " folder"
-    createDirectoryIfMissing False pastesDir
+    putStrLn $ "pastes will be saved to the " ++ pdir ++ " folder"
+    createDirectoryIfMissing False pdir
     runSpock 3849 (spock cfg app)
